8b740f5c3ed5c1b2a15a3696e104183c54bbb084
zhouziping <garfield654123@gmail.com>
2022-10-16 14:37:57 +0800

    for create meow function

--- first-react/src/App.js
+++ first-react/src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
-  return (
+const [A, SetA] =useState('喵');
+
+const meow=()=>{
+  SetA (function plus(prev){
+    return prev+'喵'
+  })
+}
+return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      
+      <h1>{A}</h1>
+      <button onClick={meow}>我是按鈕</button>
+      <ExpenseItem></ExpenseItem>
       </header>
     </div>
   );
